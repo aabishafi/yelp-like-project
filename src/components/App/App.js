0989e7d847ab81b4024ab6852bfdb7ea799f0cda
adam <somprogrammer@gmail.com>
2020-01-17 20:37:08 +1300

    third of yelp like project.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -26,11 +26,14 @@ const businesses = [
 ];
 
 class App extends React.Component {
+   searchYelp(term, location, sortBy) {
+      console.log(`you are searching ${term}, ${location}, ${sortBy}`);
+   }
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={businesses} />
       </div>
     );
@@ -38,3 +41,4 @@ class App extends React.Component {
 }
 
 export default App;
+
